eca91932cc08a390b13c56cf5579878e4499458f
Robin Held <robinheld@apple.com>
2022-12-01 22:55:33 -0800

    Day 2

--- AOC22-xcode/ContentView.swift
+++ AOC22-xcode/ContentView.swift
@@ -3,11 +3,83 @@ import SwiftGraph
 
 extension Mine {
     func run() {
-        loadInput("day1")
-        day1()
+        //day1()
+        day2()
         //pr("test")
     }
+    func day2() {
+        loadInput("day2")
+        let opponentOptions = ["A", "B", "C"]
+        let playerOptions = ["X", "Y", "Z"]
+        let rounds = input.components(separatedBy: "\n")
+        var totalScore = 0
+        for round in rounds {
+            let choices = round.components(separatedBy: " ")
+            if (choices.count >= 2) {
+                var choice = choices[1]
+                switch (choices[1]) {
+                case "X":
+                    var playerIndex = (opponentOptions.firstIndex(of: choices[0]) ?? 0) - 1
+                    if (playerIndex < 0) {playerIndex = 2}
+                    choice = playerOptions[playerIndex]
+                case "Y":
+                    var playerIndex = (opponentOptions.firstIndex(of: choices[0]) ?? 0)
+                    choice = playerOptions[playerIndex]
+                case "Z":
+                    var playerIndex = (opponentOptions.firstIndex(of: choices[0]) ?? 0) + 1
+                    if (playerIndex > 2) {playerIndex = 0}
+                    choice = playerOptions[playerIndex]
+                default:
+                    choice = choices[1]
+                }
+                let roundScore = determineOutcome(choices[0], choice)
+                totalScore += roundScore
+            }
+        }
+        pr(totalScore)
+    }
+    
+    func determineOutcome(_ opponent: String, _ choice: String) -> Int {
+        var score = 0
+        switch choice {
+        case "X":
+            score += 1
+            switch opponent {
+            case "A":
+                score += 3
+            case "C":
+                score += 6
+            default:
+                score += 0
+            }
+        case "Y":
+            score += 2
+            switch opponent {
+            case "B":
+                score += 3
+            case "A":
+                score += 6
+            default:
+                score += 0
+            }
+        case "Z":
+            score += 3
+            switch opponent {
+            case "C":
+                score += 3
+            case "B":
+                score += 6
+            default:
+                score += 0
+            }
+        default:
+            score += 0
+        }
+        return score
+    }
+    
     func day1() {
+        loadInput("day1")
         let components = input.components(separatedBy: "\n\n")
         var maxValue = 0
         var totals = [Int]()
